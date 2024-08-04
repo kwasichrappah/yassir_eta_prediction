74872e203a06fe01ff67fd313be4aa18a0e72796
chrappah <chrappahkwasi@gmail.com>
2024-08-04 21:40:56 +0000

    frontend_update

--- frontend/pages/3_Dashboard.py
+++ frontend/pages/3_Dashboard.py
@@ -24,33 +24,47 @@ def loader():
     bar.progress(i + 1)
     time.sleep(0.1)
 
-df= pd.read_csv("./data/train_data.csv")
+df= pd.read_csv("./data/train_data.csv",parse_dates = ['Timestamp'])
+df['date'] = df['Timestamp'].dt.date
+df['hour'] = df['Timestamp'].dt.hour
+df= df.sort_values(by='Timestamp')
 
 
 def eda_dashboard():
    st.markdown('### Exploratory Data Analysis Dashboard')
-   loader()
    col1,col3 = st.columns(2)
 
    with col1:
-      int_service_histogram = px.histogram(df,x='Tripdistance',color = 'Churn')
+      data = df.head(50000)
+      trip_distance_histogram = px.bar(data,x='date',y='Trip_distance',hover_data=['ETA'],color='ETA',title="Daily total trip distances")
 
-      st.plotly_chart(int_service_histogram)
+      st.plotly_chart(trip_distance_histogram)
 
    with col3:
-       contract_histogram = px.histogram(df,x='ETA',color = 'Churn')
+       eta_barchart = px.bar(data,x='date',y='ETA',hover_data =['Trip_distance'],color='Trip_distance',title="Daily sum of ETA")
 
-       st.plotly_chart(contract_histogram)  
+       st.plotly_chart(eta_barchart)  
 
+   eta_distance_barchart = px.bar(data,x='ETA',y='Trip_distance',hover_data =['hour'],color ="hour",title="ETA against Trip Distance")
+
+   st.plotly_chart(eta_distance_barchart) 
+
+
+def pickups():
    
+   pickups = df[['Origin_lat','Origin_lon']].head(1000)
+   pickups.rename(columns={"Origin_lat": "lon", "Origin_lon": "lat"},inplace=True)
+   st.map(pickups,color= '#00ff00',size = 10)
+
+def dropoffs():
+   dropoffs = df[['Destination_lat','Destination_lon']].head(1000)
+   dropoffs.rename(columns={"Destination_lat": "lon", "Destination_lon": "lat"},inplace=True) 
+   st.map(dropoffs,color= '#ff0000',size = 10)
 
 
 
-
-
-
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+# with open('frontend/config.yaml') as file:
+#     config = yaml.load(file, Loader=SafeLoader)
 
 
 if __name__ == '__main__':
@@ -59,39 +73,50 @@ if __name__ == '__main__':
 
 
 
-   authenticator = stauth.Authenticate(
-   config['credentials'],
-   config['cookie']['name'],
-   config['cookie']['key'],
-   config['cookie']['expiry_days'],
-   config['pre-authorized']
-   )
+#    authenticator = stauth.Authenticate(
+#    config['credentials'],
+#    config['cookie']['name'],
+#    config['cookie']['key'],
+#    config['cookie']['expiry_days'],
+#    config['pre-authorized']
+#    )
 
 
-authenticator.login(location='sidebar')
+# authenticator.login(location='sidebar')
 
-if st.session_state["authentication_status"]:
-   authenticator.logout(location = 'sidebar')
-   st.write(f'Welcome *{st.session_state["name"]}*')
-   st.title("Dashboard")
+# if st.session_state["authentication_status"]:
+#    authenticator.logout(location = 'sidebar')
+#    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title("Dashboard")
 
-   col1,col2 = st.columns(2)
-   with col1:
-      pass
-   with col2:
-      st.selectbox('Select the type of Dashboard',options=['EDA'],key='dashboard_type')
+    col1,col2 = st.columns(2)
+    with col1:
+        pass
+    with col2:
+        st.selectbox('Select the type of Dashboard',options=['EDA','LOCATIONS'],key='dashboard_type')
 
-   if st.session_state['dashboard_type'] == 'EDA':
-    eda_dashboard()
+    if st.session_state['dashboard_type'] == 'EDA':
+        eda_dashboard()
 
-   else:
-    pass
+    else:
+        col1,col2 = st.tabs(["Pick up points", "Dropoff Points"])
+
+        with col1:
+            st.write ('### Information on some Pickup points')
+            pickups()
+            
+            
+            
+        with col2:
+            st.write('### Information on some Dropoff points')
+            dropoffs()
+            
 
     
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+# elif st.session_state["authentication_status"] is False:
+#     st.error('Username/password is incorrect')
+# elif st.session_state["authentication_status"] is None:
+#     st.warning('Please enter your username and password')
 
 
 
@@ -100,11 +125,11 @@ elif st.session_state["authentication_status"] is None:
 
 
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How I can be contacted?',
-    ('chrappahkwasi@gmail.com','chrappahkwasi@gmail.com', '0209100603')
-)
+# # Add a selectbox to the sidebar:
+# add_selectbox = st.sidebar.selectbox(
+#     'How I can be contacted?',
+#     ('chrappahkwasi@gmail.com','chrappahkwasi@gmail.com', '0209100603')
+# )
         
 
 
